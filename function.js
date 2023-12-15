30891337c92f9166ebd9712e4df61eb0d7b5988b
Sutreke <tapiaonatejuancarlos@gmail.com>
2023-12-15 17:30:59 -0300

    segundo commit

--- function.js
+++ function.js
@@ -6,3 +6,22 @@ document.addEventListener('DOMContentLoaded', function () {
         },
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var parrafo = document.getElementById("miParrafoUno");
+    var textoCompleto = parrafo.innerHTML;
+    parrafo.innerHTML = ""; // Limpiamos el contenido original
+    var cont = 0;
+
+    function escribirTexto(index) {
+        if (index < textoCompleto.length) {
+            parrafo.innerHTML += textoCompleto.charAt(index);
+            index++;
+            setTimeout(function () {
+                escribirTexto(index);
+            }, 50); // Ajusta el tiempo entre caracteres (en milisegundos)
+        }
+    }
+
+    escribirTexto(0);
+});
