afbdeb174b644274258f538292a7190c2f9e15ec
Sutreke <tapiaonatejuancarlos@gmail.com>
2023-12-18 03:13:59 -0300

    first commit

--- function.js
+++ function.js
@@ -9,19 +9,54 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener("DOMContentLoaded", function () {
     var parrafo = document.getElementById("miParrafoUno");
-    var textoCompleto = parrafo.innerHTML;
-    parrafo.innerHTML = ""; // Limpiamos el contenido original
+    var textoCompleto = 'Mi Amor, \n\n'+
+    'En la calidez de estas palabras, '+ 
+    'encuentro el eco de mis sentimientos ' + 
+    'más profundos, una melodía que resuena ' + 
+    'en el rincón más sagrado de mi corazón. ' + 
+    'Es difícil expresar con simples letras la ' + 
+    'inmensidad de lo que siento por ti, pero hoy '+ 
+    ', en esta página, intentaré pintar con palabras '+ 
+    'el lienzo de nuestro amor. \n\n '+ 
+    'Cada instante que compartimos se convierte en '+ 
+    'un tesoro atemporal, una joya que brilla con ' + 
+    'la luz única de nuestra complicidad. En la ' + 
+    'danza eterna de la vida, tú has sido mi pareja '+ 
+    'perfecta, mi confidente y mi lugar seguro. \n\n'+ 
+    'En este rincón de la existencia donde nuestros '+ 
+    'destinos se entrelazaron, floreció un amor que ' + 
+    'va más allá de las palabras comunes, un amor que '+ 
+    'respira con cada latido de nuestro ser. \n\n'+ 
+    'Deseo expresarte mi ' + 
+    'gratitud por cada risa compartida, por cada ' + 
+    'lágrima secada y por el crecimiento que hemos '+ 
+    'experimentado juntos. Eres la melodía que adorna ' + 
+    'mi vida, la poesía que da sentido a mis días, y cada ' + 
+    'línea escrita lleva consigo la esencia de mi amor '+  
+    'incondicional hacia ti.';
+
     var cont = 0;
+    indice = 0;
 
-    function escribirTexto(index) {
-        if (index < textoCompleto.length) {
-            parrafo.innerHTML += textoCompleto.charAt(index);
-            index++;
-            setTimeout(function () {
-                escribirTexto(index);
-            }, 20); // Ajusta el tiempo entre caracteres (en milisegundos)
+    
+
+    function escribirTexto(textoCompleto) {
+        if (typeof parrafo !== 'undefined') {
+            parrafo.innerHTML += textoCompleto.charAt(indice);
+    
+            if (textoCompleto.charAt(indice) === '\n') {
+                parrafo.innerHTML += '<br>';
+            }
+    
+            indice++;
+    
+            if (indice < textoCompleto.length) {
+                setTimeout(function() {
+                    escribirTexto(textoCompleto);
+                }, 20);
+            }
+          }
         }
-    }
 
-    escribirTexto(0);
+    escribirTexto(textoCompleto);
 });
